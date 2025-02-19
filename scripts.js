c73a7e5e585150f72a6e5859b4c39baf5b2a4eec
joao12243 <150459953+joao12243@users.noreply.github.com>
2025-02-19 13:31:41 -0300

    Criando o ícone de remover

--- scripts.js
+++ scripts.js
@@ -83,8 +83,14 @@ function expenseAdd(newExpense) {
       .replace("R$", "")
     }`
 
+    // Cria o icone de remover.
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute("src", "img/remove.svg")
+    removeIcon.setAttribute("alt", "remover")
+
     // Adiciona as informações no item.
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
     // Adiciona o item na lista.
     exepenseList.append(expenseItem)
